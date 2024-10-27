ba11778d9526d5697f6558aed656a14a7aee344f
janjanjan312 <1262325196@qq.com>
2024-10-27 21:21:16 +0000

    done

---
+++ app/controllers/zebra_controller.rb
@@ -0,0 +1,55 @@
+class ZebraController < ApplicationController
+  def giraffe
+    @comp_move = ["rock","paper","scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "tied"
+    elsif @comp_move == "paper"
+      @outcome = "lost"
+    elsif @comp_move == "scissors"
+      @outcome = "won"
+    end
+    
+    render({:template => "game_templates/play_rock"})
+  end
+
+
+  def elephant
+    @comp_move = ["rock","paper","scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "won"
+    elsif @comp_move == "paper"
+      @outcome = "tied"
+    elsif @comp_move == "scissors"
+      @outcome = "lost"
+    end
+    
+    render({:template => "game_templates/play_paper"})
+
+
+  end
+
+
+  def lion
+
+    @comp_move = ["rock","paper","scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "lost"
+    elsif @comp_move == "paper"
+      @outcome = "won"
+    elsif @comp_move == "scissors"
+      @outcome = "tied"
+    end
+    
+    render({:template => "game_templates/play_scissors"})
+
+    
+  end
+
+
+  def rules
+    render({:template => "game_templates/homepage"})
+  end
+end
